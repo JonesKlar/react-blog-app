ec17ebcdfc47e130095366c1bbb5a309409403f8
Admin <Admin@BUSHIDO>
2025-05-17 10:47:19 +0200

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/react-blog-app/',
+  build: {
+    outDir: "build", // Change 'dist' to 'build'
+  },
   server: {
     host: true,      // shorthand for 0.0.0.0
     port: 5173,
